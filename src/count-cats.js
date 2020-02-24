fcfdce59b7da0b8e8948f9d7e911690a5a552772
Alexander <snarsky93@gmail.com>
2020-02-24 20:07:22 +0300

    add solution of cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(elem => elem.forEach(item => (item === '^^') ? count++ : count));
+  return count;
 };
