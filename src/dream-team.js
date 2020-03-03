7e091344377225b0dc48b142a7f330d696ea0bbd
Alexander <snarsky93@gmail.com>
2020-03-03 15:50:45 +0300

    feat: add solution of dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,25 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  
+  if (typeof(members) === 'number' || typeof(members) === 'boolean' || !members || Array.isArray(members) === false) {
+    return false;
+  } 
+  
+ if (members == []) {
+    throw new UserException("error");
+  }
+   
+    let k = '';
+
+      members.forEach(elem => {
+        if (typeof(elem) === 'number' || typeof(elem) === 'boolean' || typeof(elem) === 'object' || elem === null || elem === undefined) {
+
+             return false;
+        } else {
+          k += elem.trim().toUpperCase().replace(/\s+/g, '').split('').slice(0,1).join('');
+        };
+          });
+
+       let result = k.split('').sort().join('');
+       return result;
+  };
+  
